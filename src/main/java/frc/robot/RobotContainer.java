386d53b2fa2b8ed3504af7cec453c28bef90910d
Scott Jasperse <scott.jasperse@gmail.com>
2023-03-21 19:36:58 -0400

    Untested shooter auton

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -78,7 +78,7 @@ public class RobotContainer {
   Supplier<Boolean> isRedAllianceSupplier = () -> NetworkTableInstance.getDefault().getEntry("/FMSInfo/IsRedAlliance").getBoolean(false);
   private final Auton3 m_balanceAuton = new Auton3(m_robotDrive, m_intake, m_gyro);
   private final Auton4 m_driveAuton = new Auton4(m_robotDrive, m_intake, m_gyro);
-  private final Auton5 m_testAuton = new Auton5(m_robotDrive, m_intake, m_gyro);
+  private final Auton5 m_testAuton = new Auton5(m_robotDrive, m_intake, m_gyro, m_shooter);
   SendableChooser<Command> m_autonChooser = new SendableChooser<>();
 
   private final SendableChooser<DriverProfileBase> m_driverChooser = new SendableChooser<>();
--- src/main/java/frc/robot/commands/Autonomous/Auton5.java
+++ src/main/java/frc/robot/commands/Autonomous/Auton5.java
@@ -1,6 +1,9 @@
 package frc.robot.commands.Autonomous;
 
 import com.kauailabs.navx.frc.AHRS;
+
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.commands.BalanceCommand;
@@ -8,11 +11,14 @@ import frc.robot.commands.DriveDistanceCommand;
 import frc.robot.commands.Intake.IntakeSpeedCommand;
 import frc.robot.subsystems.Drivetrain;
 import frc.robot.subsystems.Intake;
+import frc.robot.subsystems.Shooter;
 
 public class Auton5 extends SequentialCommandGroup {
-    public Auton5(Drivetrain drivetrain, Intake intake, AHRS gyro) {
-        addRequirements(drivetrain, intake);
+    public Auton5(Drivetrain drivetrain, Intake intake, AHRS gyro, Shooter shooter) {
+        addRequirements(drivetrain, intake, shooter);
         addCommands(
+            new RunCommand(() -> { shooter.shooterEject(-0.8); }).withTimeout(5),
+            new InstantCommand(() -> { shooter.shooterStop(); }),
             //start up intake to eject game piece
             new IntakeSpeedCommand(0.5, 1, intake),
             //wait 1 second
